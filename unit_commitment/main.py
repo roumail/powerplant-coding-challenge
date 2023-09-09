9c012b440367cef9fe5ea8e8373ae1af4e4b4171
Rohail Taimour <rohail.taimour@gmail.com>
2023-09-09 20:32:17 +0200

    Running entrypoint

--- unit_commitment/main.py
+++ unit_commitment/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 from unit_commitment.api.routes.api import router as api_router
 from unit_commitment.core.config import settings
@@ -13,3 +14,16 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
+
+def main():
+    run(
+        "unit_commitment.main:app",
+        host="0.0.0.0",
+        port=8888,
+        reload=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
