e222ee25e8ec20134e2508b53222a3572a2379e1
Rohail Taimour <rohail.taimour@gmail.com>
2023-09-10 03:29:01 +0200

    working prototype

--- unit_commitment/production_plan/greedy.py
+++ unit_commitment/production_plan/greedy.py
@@ -23,33 +23,44 @@ def calculate_fuel_cost(plant: "PowerPlant", fuels: "Fuel"):
 def allocate_power(
     load: int, fuels: "Fuel", powerplants: list["PowerPlant"]
 ) -> list[ResponsePowerPlant]:
-    # Step 1: Sort Power Plants by Cost
     plant_fuel_costs = {}
     for plant in powerplants:
         plant_fuel_costs[plant.name] = calculate_fuel_cost(plant, fuels)
     sorted_plants = sorted(powerplants, key=lambda x: plant_fuel_costs[x.name])
 
-    # Initialize variables
-    allocated_power = []
+    allocated_power = {}
     remaining_load = load
 
-    # Step 2: Fulfill Minimum Requirements
+    # First, allocate to plants with zero pmin and lowest cost
+    for plant in sorted_plants:
+        if plant.pmin == 0:
+            power_to_allocate = min(plant.pmax, remaining_load)
+            allocated_power[plant.name] = power_to_allocate
+            remaining_load -= power_to_allocate
+            if remaining_load <= 0:
+                break
+
+    # Then, fulfill minimum requirements
     for plant in sorted_plants:
         if plant.pmin > 0:
-            allocated_power.append(ResponsePowerPlant(name=plant.name, p=plant.pmin))
+            allocated_power[plant.name] = plant.pmin
             remaining_load -= plant.pmin
 
-    # Step 3 and 4: Allocate Remaining Load and Iterate
-    i = 0
-    while remaining_load > 0 and i < len(sorted_plants):
-        plant = sorted_plants[i]
-        additional_power = min(plant.pmax - allocated_power[i].p, remaining_load)
-        allocated_power[i].p += additional_power
-        remaining_load -= additional_power
-        i += 1
+    # Finally, allocate remaining load
+    for plant in sorted_plants:
+        if remaining_load <= 0:
+            break
 
-    # Check if all demand is met
+        current_allocation = allocated_power.get(plant.name, 0)
+        additional_power = min(plant.pmax - current_allocation, remaining_load)
+
+        allocated_power[plant.name] = current_allocation + additional_power
+        remaining_load -= additional_power
+
     if remaining_load > 0:
         print("Warning: Not all demand could be met.")
 
-    return allocated_power
+    # Convert the allocated_power dictionary to a list of ResponsePowerPlant objects
+    result = [ResponsePowerPlant(name=name, p=p) for name, p in allocated_power.items()]
+
+    return result
